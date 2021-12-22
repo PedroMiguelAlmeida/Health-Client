dcc1055fa25c4024e76df6753c4af2048a2de01d
¨PedroMiguelAlmeida¨ <pedromiguel.almd@gmail.com>
2021-12-22 00:52:45 +0000

    Added Administrator and Health professional constructors,getters&setters

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ProjetoDae/entities/Administrator.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ProjetoDae/entities/Administrator.java
@@ -1,7 +1,27 @@
 package pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.entities;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
+
+@Entity
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllAdministrators",
+                query = "SELECT s FROM User s ORDER BY s.name" // JPQL
+        )
+})
 public class Administrator extends User implements Serializable {
+
+    //region constructor
+    public Administrator() {
+    }
+
+    public Administrator(String username, String password, String name, String email, int version) {
+        super(username, password, name, email, version);
+    }
+//endregion
 
 }
--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ProjetoDae/entities/HealthProfessional.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ProjetoDae/entities/HealthProfessional.java
@@ -1,11 +1,60 @@
+
+
+
 package pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import java.io.Serializable;
 import java.util.List;
 
+
+@Entity
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllHealthProfessionals",
+                query = "SELECT s FROM User s ORDER BY s.name" // JPQL
+        )
+})
 public class HealthProfessional extends User implements Serializable {
+    //region attributes
+    private String profession;
+    private boolean chefe;
 
 
-    private List<Patient> patients;
+//    private List<Patient> patients;
+//endregion attributes
 
+    //region constructors
+    public HealthProfessional() {
+    }
+
+    public HealthProfessional(String username, String password, String name, String email, int version, String profession,  boolean chefe) {
+        super(username, password, name, email, version);
+        this.profession = profession;
+        this.chefe = chefe;
+    }
+//endregion constructors
+
+    //region getters&setters
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+
+    public boolean isChefe() {
+        return chefe;
+    }
+
+    public void setChefe(boolean chefe) {
+        this.chefe = chefe;
+    }
+
+//endregion
 }
+
