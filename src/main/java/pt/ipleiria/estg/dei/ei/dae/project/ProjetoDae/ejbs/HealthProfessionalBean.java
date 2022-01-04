a5be5472029b82dfaa77bfafc143e648e3bcdf3c
¨PedroMiguelAlmeida¨ <pedromiguel.almd@gmail.com>
2022-01-04 14:44:48 +0000

    Token with crete system implemented for HealthProfessional and Patient

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ProjetoDae/ejbs/HealthProfessionalBean.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/project/ProjetoDae/ejbs/HealthProfessionalBean.java
@@ -5,29 +5,52 @@ import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.Roles;
 import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.entities.Administrator;
 import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.entities.HealthProfessional;
 import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.entities.Patient;
+import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.entities.Token;
+import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.exceptions.MyConstraintViolationException;
+import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.exceptions.MyEntityExistsException;
 import pt.ipleiria.estg.dei.ei.dae.project.ProjetoDae.exceptions.MyEntityNotFoundException;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.mail.MessagingException;
 import javax.persistence.EntityManager;
 import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
+import javax.ws.rs.NotAuthorizedException;
 import java.util.List;
+import java.util.Objects;
 
 @Stateless
 public class HealthProfessionalBean {
     @PersistenceContext
     private EntityManager em;
-    public void create(String username, String password, String name, String email, int version, String profession, boolean chefe, Roles role,boolean active){
+    @EJB
+    private EmailBean emailBean;
+    @EJB
+    private TokenBean tokenBean;
+
+
+    public void create(String username, String password, String name, String email, int version, String profession, boolean chefe, Roles role,boolean active) throws MyConstraintViolationException, MyEntityNotFoundException, MyEntityExistsException, MessagingException {
 
         HealthProfessional professional = (HealthProfessional)this.em.find(HealthProfessional.class, username);
         if (professional != null) {
             System.out.println("HealthProfessional with username: " + username + " already exists");
             System.exit(0);
         }
-         professional = new HealthProfessional(username,password,name,email,version,profession,chefe,role,active);
-        em.persist(professional);
-        if(professional == null){
-            System.out.println("ERROR! creating Professional");
+        try {
+            professional = new HealthProfessional(username, password, name, email, version, profession, chefe, role, active);
+            em.persist(professional);
+            tokenBean.create(email);
+            Token token = tokenBean.findToken(email);
+            System.out.println("This is email: "+ email);
+            if (token == null){
+                throw new MyEntityNotFoundException("token not found");
+            }
+            emailBean.send(email,"localhost:3000/"+token.getToken(),token.getToken());
+        }
+        catch (ConstraintViolationException e){
+            throw new MyConstraintViolationException(e);
         }
     }
 
@@ -58,6 +81,20 @@ public class HealthProfessionalBean {
 
     public List<HealthProfessional> getAllHealthProfessionals(){
         return (List<HealthProfessional>) em.createNamedQuery("getAllHealthProfessionals").getResultList();
+    }
+
+    public void updatePassword(String username,String password,String tokenString) throws MyEntityNotFoundException {
+        HealthProfessional healthProfessional = em.find(HealthProfessional.class,username);
+        if (healthProfessional==null){
+            throw new MyEntityNotFoundException("HealthProfessional not found");
+        }
+        Token token = tokenBean.findToken(healthProfessional.getEmail());
+        System.out.println("token1: "+token.getToken()+"token2: "+tokenString );
+        if (!Objects.equals(token.getToken(), tokenString)){
+            throw new NotAuthorizedException("Token was not found");
+        }
+        em.lock(healthProfessional,LockModeType.OPTIMISTIC);
+        healthProfessional.setPassword(password);
     }
 
     public HealthProfessional findHealthProfessional(String username){return (HealthProfessional)this.em.find(HealthProfessional.class,username);}
